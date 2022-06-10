e0cb638ec277c12782e34c8cac9eb2ecc27fa07b
Unsaved-Inf0 <60225472+Unsaved-Inf0@users.noreply.github.com>
2022-06-10 18:28:33 +0700

    update Player branch

---
+++ FootballScheduleManagement/FootballLeagueManagement/FootballScheduleManagement/Model/Forms/BSManagementForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FootballScheduleManagement.Model.Forms
+{
+    class BSManagementForm
+    {
+        public void RedirectForm(ref User user, Form redirectForm, int index)
+        {
+            switch(index)
+            {
+                case 1:
+                    if (user.Permission.Contains((int)PermissionOfRoles.Edit_Table_Match_Referee_Scores))
+                    {
+                        redirectForm.Dock = DockStyle.Fill;
+                        redirectForm.Show();
+                    }
+                    break;
+                case 2:
+                    if (user.Permission.Contains((int)PermissionOfRoles.Edit_Table_Player_Club))
+                    {
+                        redirectForm.Dock = DockStyle.Fill;
+                        redirectForm.Show();
+                    }
+                    break;
+                case 3:
+                    if (user.Permission.Contains((int)PermissionOfRoles.Edit_Table_Match_Referee_Scores))
+                    {
+                        redirectForm.Dock = DockStyle.Fill;
+                        redirectForm.Show();
+                    }
+                    break;
+                case 4:
+                    if (user.Permission.Contains((int)PermissionOfRoles.Edit_Table_Player_Club))
+                    {
+                        redirectForm.Dock = DockStyle.Fill;
+                        redirectForm.Show();
+                    }
+                    break;
+                case 5:
+                    if (user.Permission.Contains((int)PermissionOfRoles.Edit_Table_Match_Referee_Scores))
+                    {
+                        redirectForm.Dock = DockStyle.Fill;
+                        redirectForm.Show();
+                    }
+                    break;
+                case 6:
+                    if (user.Permission.Contains((int)PermissionOfRoles.Enter_Normal_Form))
+                        redirectForm.Show();
+                    break;
+            }
+        }
+    }
+}
---
+++ FootballScheduleManagement/FootballLeagueManagement/FootballScheduleManagement/Model/Forms/BSMatchNormalForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.XtraGrid;
+using FootballScheduleManagement.DB.AdapterPattern;
+
+namespace FootballScheduleManagement.Model.Forms
+{
+    class BSMatchNormalForm
+    {
+        IDBHandler db;
+        DataSet dataSet;
+        DataTable dataTable;
+        SqlCommand sqlCommand;
+        public BSMatchNormalForm()
+        {
+            db = new SqlServerAdapter(new SqlServer());
+            sqlCommand = new SqlCommand();
+            dataSet = new DataSet();
+            dataTable = new DataTable();
+        }
+        public void LoadData(ref GridControl dataGridView)
+        {
+            sqlCommand.Parameters.Clear();
+            string sql = "SELECT * FROM Match";
+            sqlCommand.CommandText = sql;
+            dataSet = db.ExcecuteDataQuery(sqlCommand);
+            dataTable = dataSet.Tables[0];
+            dataGridView.DataSource = dataTable;
+        }
+    }
+}
